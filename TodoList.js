f1b996968ca6127376f368509c44220f9988aa75
Alexey Chernov <alexchernov063@gmail.com>
2026-03-26 18:12:37 +0100

    added add and list methods to ToDoList class

--- TodoList.js
+++ TodoList.js
@@ -3,4 +3,12 @@ export class ToDoList {
     constructor() {
         this.tasks = []
     }
+    // Add task
+    addTask(task) {
+        this.tasks.push(task)
+        }
+    // List all tasks
+    listTasks() {
+        return this.tasks
+    }
 }
