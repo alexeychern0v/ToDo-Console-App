380de0f6a4ad70281b0029db43efb5059d055f42
Alexey Chernov <alexchernov063@gmail.com>
2026-03-26 19:20:32 +0100

    added process argv and switch to work with commands entered by user

--- TodoList.js
+++ TodoList.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { Task } from './Task.js'
 
 
 // created Class ToDoList 
--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-import { Task } from "./Task.js";
 import { ToDoList } from "./TodoList.js";
 
-const task1 = new Task(1, 'Buy Milk')
-const task2 = new Task(2, 'Learn JS')
-const list1 = new ToDoList()
 
-list1.addTask(task1)
-list1.addTask(task2)
-console.log(list1.listTasks())
-list1.deleteTask(1)
-console.log(list1.listTasks())
-list1.completeTask(2)
-console.log(list1.listTasks())
+const list1 = new ToDoList()
+list1.loadTasks()
+
+const command = process.argv[2]
+const value = process.argv[3]
+
+switch (command) {
+    case 'add':
+        list1.addTask(value)
+        console.log('Task added!')
+        break
+
+    case 'list':
+        console.log(list1.listTasks())
+        break
+
+    case 'complete':
+        list1.completeTask(value)
+        console.log('Task completed!')
+        break
+
+    case 'delete':
+        list1.deleteTask(value)
+        console.log('Task deleted!')
+        break
+
+    default:
+        console.log('Unknown command. Use add / delete / complete / list commands.')
+}
